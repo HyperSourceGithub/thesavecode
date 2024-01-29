f89a74e5f181f6464003bb7bbf7241ad66daca7a
HyperSource <135276870+HyperSourceGithub@users.noreply.github.com>
2024-01-29 17:43:30 +0000

    minor edits

--- thesavecode.py
+++ thesavecode.py
@@ -1,14 +1,29 @@
+import random
+import time
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 savecode = None
 count = 0
 hamburger = None
 bullets = 0
 
+def generate_randoms(counter):
+  for x in range(counter):
+    rand = f"{random.randint(0,counter)}{x}{random.choice(letters)}{random.choice(letters)}{random.choice(letters)}{random.choice(letters).upper()}{random.choice(letters).upper()}{random.choice(letters).upper()}{random.choice(letters).upper()}"
+  return rand
+
 mode = input("Enter mode (read/write): ").lower()
 
 if mode == "write":
   upto = input("Enter a number to count up to (max 4 digits): ")
-  while (count < upto[1:4]):
-    print(f"Count: {count}", end = '\r')
+  maxcount = upto[0:4]
+  print(" ")
+  while (count < int(maxcount)):
+    print ("\033[A\033[A")
+    count += 1
+    print(f"Count: {count}")
+
 
   food = input("Enter yes/no: ").lower()
   if food == "yes":
@@ -19,11 +34,15 @@ if mode == "write":
     print("Error: invalid answer, defaulting to yes")
 
   load = input("Enter magazine bullet count for pistol (max 4 digits): ")
-  while (bullets < load[1:4]):
-    print(f"Bullets: {bullets}", end = '\r')
+  maxbullets = load[0:4]
+  print(" ")
+  while (bullets < int(maxbullets)):
+    print ("\033[A\033[A")
+    bullets += 1
+    print(f"Bullets: {bullets}")
 
   print("Generating save code...")
-  savecode = f"{count}{hamburger}{bullets}"
+  savecode = f"{count}{hamburger}{bullets}{generate_randoms(random.randint(0,50000))}"
   print(f"Save code: {savecode}")
 
 elif mode == "read":
